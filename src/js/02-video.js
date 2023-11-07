da85943b2800ee2affe7595d6eef8a844a20530c
Borys-Grinberg <borys.grinberg@gmail.com>
2023-11-07 19:27:01 +0100

    video player commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+const localStorage = window.localStorage;
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(parseFloat(currentTime));
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime.toString());
+  }, 1000)
+);
+
+player.play();
